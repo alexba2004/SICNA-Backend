5394a2957164dca07b9474abb02efdee89447602
Briones <j.alejandro.briones.a22@gmail.com>
2024-02-25 14:32:49 -0600

    add: SICNA Backend project

---
+++ src/main/java/com/briones/sicnabackend/SicnaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.briones.sicnabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicnaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicnaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briones/sicnabackend/SicnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briones.sicnabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
