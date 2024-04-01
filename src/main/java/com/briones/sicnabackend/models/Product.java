b1cbce7a2cf69d0da2b63b52a0a3c14aa8279865
Briones <j.alejandro.briones.a22@gmail.com>
2024-04-01 14:39:20 -0600

    add: product model

---
+++ src/main/java/com/briones/sicnabackend/models/Product.java
@@ -0,0 +1,121 @@
+package com.briones.sicnabackend.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column
+    private String description;
+
+    @Column
+    private String image;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pertenencia_id", nullable = false)
+    private Space space;
+
+    @Column(nullable = false)
+    private Date registrationDate;
+
+    @Column(nullable = false)
+    private Date modificationDate;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Space getSpace() {
+        return space;
+    }
+
+    public void setSpace(Space space) {
+        this.space = space;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
