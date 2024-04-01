6d158b90f1de26fb886c1854d16c4553a48c0052
Briones <j.alejandro.briones.a22@gmail.com>
2024-04-01 14:35:21 -0600

    add: area model

---
+++ src/main/java/com/briones/sicnabackend/models/Area.java
@@ -0,0 +1,66 @@
+package com.briones.sicnabackend.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Date registrationDate;
+
+    @Column(nullable = false)
+    private Date modificationDate;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
+
