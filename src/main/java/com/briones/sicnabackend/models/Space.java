39ef82a221aa9dbd8ae18009e33850f2d377aab5
Briones <j.alejandro.briones.a22@gmail.com>
2024-04-01 14:38:10 -0600

    add: space model

---
+++ src/main/java/com/briones/sicnabackend/models/Space.java
@@ -0,0 +1,90 @@
+package com.briones.sicnabackend.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Space {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "area_superior_id", nullable = false)
+    private Department department;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "persona_encargada_id", nullable = false)
+    private Person responsiblePerson;
+
+    @Column(nullable = false)
+    private Date registrationDate;
+
+    @Column(nullable = false)
+    private Date modificationDate;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Person getResponsiblePerson() {
+        return responsiblePerson;
+    }
+
+    public void setResponsiblePerson(Person responsiblePerson) {
+        this.responsiblePerson = responsiblePerson;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
+
