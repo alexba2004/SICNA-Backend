0d2af0bd723bece3556d710a75e14ed1afb16aac
Briones <j.alejandro.briones.a22@gmail.com>
2024-04-02 11:07:55 -0600

    fix: models

--- src/main/java/com/briones/sicnabackend/SicnaBackendApplication.java
+++ src/main/java/com/briones/sicnabackend/SicnaBackendApplication.java
@@ -2,13 +2,14 @@ package com.briones.sicnabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.briones.sicnabackend", "com.briones.lib.seeders", "com.briones.controllers", "com.briones.config"}, exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
+@EntityScan("com.briones.sicnabackend.models")
+@EnableJpaRepositories("com.briones.sicnabackend.repositories")
 public class SicnaBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SicnaBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SicnaBackendApplication.class, args);
+    }
 }
--- src/main/java/com/briones/sicnabackend/models/Department.java
+++ src/main/java/com/briones/sicnabackend/models/Department.java
@@ -1,6 +1,6 @@
 package com.briones.sicnabackend.models;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
